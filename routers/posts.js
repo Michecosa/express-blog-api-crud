f7c71a7ef48efe99d7654cc4e314f12d302feb09
Michela <micheladellagatta1@gmail.com>
2025-11-26 11:48:37 +0100

    Refactor posts router to use controller methods

--- routers/posts.js
+++ routers/posts.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../data/posts");
 
-router.get("/", (req, res) => {
-  res.json(posts);
-});
-
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const post = posts.find((p) => p.id === id);
-  res.json(post);
-});
-
-router.post("/", (req, res) => {
-  res.send("Creazione di un nuovo post");
-});
-
-router.put("/:id", (req, res) => {
-  res.send(`Modifica del post ${req.params.id}`);
-});
-
-router.delete("/:id", (req, res) => {
-  res.send(`Cancellazione del post ${req.params.id}`);
-});
+const {
+  index,
+  show,
+  store,
+  update,
+  destroy,
+} = require("../controllers/postController");
 
 module.exports = router;
